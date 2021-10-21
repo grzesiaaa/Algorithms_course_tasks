fa2f163af929f4c0a9e1105f97cacb6acaea73c2
Julia Grzegorzewska <julek0309@gmail.com>
2021-10-21 14:42:52 +0200

    first version

--- pythagorean.py
+++ pythagorean.py
@@ -1,9 +1,43 @@
-def pythagorean_triple(l):
-    for a in range(1,l):
+import time
+
+def pythagorean_triple_1(l):
+    for a in range(1, l):
         for b in range(1, l):
             for c in range(1, l):
-                if a**2 + b**2 == c **2 and a+b+c==l:
-                    print(a,b,c)
+                if a ** 2 + b ** 2 == c ** 2 and a + b + c == l:
+                    return a, b, c
 
 
-pythagorean_triple(1000)
+def pythagorean_triple_2(l):
+    for a in range(1, l):
+        for b in range(1, l):
+            c = l - a - b
+            if a ** 2 + b ** 2 == c ** 2 and a + b + c == l:
+                return a, b, c
+
+
+def pythagorean_triple_3(l):
+    for a in range(1, l):
+        for b in range(a, l):
+            c = l - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                return a, b, c
+
+
+def pythagorean_triple_4(l):
+    for a in range(1, l//2):
+        for b in range(a, l//2):
+            c = l - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                return a, b, c
+
+
+def pythagorean_triple_5(l):
+    for a in range(1, l//3):
+        b = (2*a*l-l**2)//(2*(a-l))
+        c = l - a - b
+        if a ** 2 + b ** 2 == c ** 2:
+            return a, b, c
+
+
+print(pythagorean_triple_5(4392))
