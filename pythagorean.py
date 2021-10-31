b8160e78043f24b05871b1b4de0ef91bfe12e902
Julia Grzegorzewska <julek0309@gmail.com>
2021-10-31 15:41:55 +0100

    Update pythagorean.py

--- pythagorean.py
+++ pythagorean.py
@@ -1,4 +1,6 @@
 import time
+import math
+
 
 def pythagorean_triple_1(l):
     operations = 0
@@ -14,17 +16,6 @@ def pythagorean_triple_1(l):
 def pythagorean_triple_2(l):
     operations = 0
     for a in range(1, l):
-        for b in range(1, l):
-            operations += 11
-            c = l - a - b
-            if a ** 2 + b ** 2 == c ** 2 and a + b + c == l:
-                return True, a, b, c, operations
-    else:
-        return False, None, None, None, operations
-
-def pythagorean_triple_3(l):
-    operations = 0
-    for a in range(1, l):
         for b in range(a, l):
             operations += 7
             c = l - a - b
@@ -33,7 +24,7 @@ def pythagorean_triple_3(l):
     else:
         return False, None, None, None, operations
 
-def pythagorean_triple_4(l):
+def pythagorean_triple_3(l):
     operations = 0
     for a in range(1, l//2):
         for b in range(a, l//2):
@@ -44,24 +35,37 @@ def pythagorean_triple_4(l):
     else:
         return False, None, None, None, operations
 
-def pythagorean_triple_5(l):
+def pythagorean_triple_4(l):
     operations = 0
     for a in range(1, l//3):
         operations += 14
-        b = (2*a*l-l**2)//(2*(a-l))
+        b = (2*a*l-l*l)//(2*(a-l))
         c = l - a - b
-        if a ** 2 + b ** 2 == c ** 2:
+        if a * a + b * b == c * c:
             return True, a, b, c, operations
     else:
         return False, None, None, None, operations
 
-"""start = time.time()
-print(pythagorean_triple_4(1000))
-print(time.time()-start)"""
 
 start = time.time()
-for i in range(1,1000):
-    pythagorean_triple_5(1000)
-print((time.time()-start)/1000)
-print(pythagorean_triple_5(1000))
-#jakas tabelka w raporcie ile operacji jedna druga trzecia metoda
+for i in range(1,5000):
+    pythagorean_triple_1(10)
+print((time.time()-start)/5000)
+
+start = time.time()
+for i in range(1,5000):
+    pythagorean_triple_2(10)
+print((time.time()-start)/5000)
+
+start = time.time()
+for i in range(1,5000):
+    pythagorean_triple_3(10)
+print((time.time()-start)/5000)
+
+start = time.time()
+for i in range(1,5000):
+    pythagorean_triple_4(10)
+print((time.time()-start)/5000)
+
+
+
