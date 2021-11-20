8c8bace1d97c1b0ddb6f049512a9179ca83ead54
Julia Grzegorzewska <julek0309@gmail.com>
2021-11-20 22:15:56 +0100

    zad4

---
+++ zad4.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+def matches(op, cl):
+    opens = "<"
+    closers = ">"
+    return opens.index(op) == closers.index(cl)
+
+
+def checking_HTML_correctness(filename):
+    """
+    Funkcja ma za zadanie sprawdzać poprawność składni dokumentu HTML.
+    Jako argument przyjmuje nazwę pliku, który ma sprawdzić.
+    Zwraca True jeśli dokument jest poprawny składniowo i False jeśli nie jest.
+    """
+    s = Stack()
+    balanced = True
+    index = 0
+
+    file_obj = open(filename, 'r')
+    text = file_obj.read()
+
+    signs = []
+    for sign in text:
+        signs.append(sign)
+
+    
