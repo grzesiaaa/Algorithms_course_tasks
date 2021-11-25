5d0bbf9ba06ab9be27dfba3276d2bf3eb1e6443f
Julia Grzegorzewska <julek0309@gmail.com>
2021-11-25 12:12:35 +0100

    Update L4_ZAD2.py

--- L4_ZAD2.py
+++ L4_ZAD2.py
@@ -1,45 +1,72 @@
 from L4_ZAD1 import QueueBaE, QueueBaB
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
-def compare_implementations(n):
+def count_times(n):
     BaB = QueueBaB()
     BaE = QueueBaE()
 
     start1 = time.time()
-    for _ in range(0,1000):
-        for i in range(0, n):
-            BaB.enqueue(i)
-    time1 = (time.time()-start1)/1000
+    for i in range(0, n):
+        BaB.enqueue(i)
+    time1 = time.time()-start1
 
     start2 = time.time()
-    for _ in range(0, 1000):
-        for i in range(0, n):
-            BaE.enqueue(i)
-    time2 = (time.time()-start2)/1000
+    for i in range(0, n):
+        BaE.enqueue(i)
+    time2 = time.time()-start2
 
     start3 = time.time()
-    for _ in range(0,1000):
-        for _ in range(0, n):
-            BaB.dequeue()
-    time3 = (time.time()-start3)/1000
+    for _ in range(0, n):
+        BaB.dequeue()
+    time3 = time.time()-start3
 
     start4 = time.time()
-    for _ in range(0, 1000):
-        for _ in range(0, n):
-            BaE.dequeue()
-    time4 = (time.time() - start4) / 1000
+    for _ in range(0, n):
+        BaE.dequeue()
+    time4 = time.time() - start4
 
     times = [time1, time2, time3, time4]
-
     return times
 
-def plots(n):
 
-    for _ in range(0, n):
-        print(compare_implementations(n))
-    plt.plot(n, compare_implementations())
-print(plots(10))
+def compare_enqueue(n):
+    x1 = np.linspace(0, n)
+    BaB_enq = count_times(n)[0] * x1
+    plt.subplot(2, 1, 1)
+    plt.plot(x1, BaB_enq)
+    plt.ylim(0, n/2)
+    plt.grid()
+
+    x2 = np.linspace(0, n)
+    BaE_enq = count_times(n)[1] * x2
+    plt.subplot(2, 1, 2)
+    plt.plot(x2, BaE_enq)
+    plt.ylim(0, n/2)
+    plt.grid()
+
+    plt.show()
+
+def compare_dequeue(n):
+    x3 = np.linspace(0, n)
+    BaB_deq = count_times(n)[2] * x3
+    plt.subplot(2, 1, 1)
+    plt.plot(x3, BaB_deq)
+    plt.ylim(0, n/2)
+    plt.grid()
+
+    x4 = np.linspace(0, n)
+    BaE_deq = count_times(n)[3] * x4
+    plt.subplot(2, 1, 2)
+    plt.plot(x4, BaE_deq)
+    plt.ylim(0, n/2)
+    plt.grid()
+
+    plt.show()
+
+
+print(compare_dequeue(100000))
 
 
 
