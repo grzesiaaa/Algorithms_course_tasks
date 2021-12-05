07608a8e583130f2edb695f01afbb0a1ed0c7917
fiminka <wikifyy@gmail.com>
2021-12-06 00:35:14 +0100

    first_part(ex3)

--- L4_ZAD3.py
+++ L4_ZAD3.py
@@ -9,6 +9,8 @@ zaprojektuj i przeprowadź symulację, która udzieli na nie odpowiedzi.
 Pamiętaj o określeniu wszystkich uproszczeń swojego modelu. 
 """
 
+import random
+
 """
 Jako symulację wybrałyśmy sytuację polegającą na określeniu prędkości wpuszczania ludzi do auli na koncert 
 w zależności od tego czy ludzie byli wpuszczani pojedynczo, czy bilety można było zakupić grupowo. 
@@ -44,4 +46,58 @@ class Stack:
 
 
 class Participant:
-    def __init__(self,):
+    """ Participant contains participant's attributes.
+
+    Parameters:
+    _____________
+    number: {int}
+     number of the participant
+    place: {str}
+     place of participant, in or outside the philharmonic
+    happiness: {int}
+     amount of happiness of the participant; goes down when sees VIPParticipant
+    accompany: {int}
+     how many people accompany the participant
+    """
+    def __init__(self, number, place="out", happiness=100, accompany=0):
+        self.n = number
+        self.p = place
+        self.hap = happiness
+        self.acom = accompany
+
+    def get_inside(self):
+        """Changes place of the participant when gets inside the philharmonic"""
+        self.p = "in"
+
+    def get_angry(self):
+        self.hap -= 10
+
+
+class VIPParticipant:
+    def __init__(self, place="out"):
+        self.p = place
+
+
+class Philharmonic:
+    """Philharmonic includes how many doors are there to make queues.
+    Parameters:
+    _____________
+    doors: {int}
+     how many doors philharmonic has
+    """
+    def __init__(self, doors=1):
+        self.s = doors
+
+
+def types_of_tickets(sold):
+    list_of_participants = []
+    for i in range(sold):
+        list_of_participants.append(Participant(number=i))
+        if Participant(number=i).acom == 0:
+            i += 1
+        else:
+            i += 1 + Participant(number=i).acom
+    return list_of_participants
+
+
+def
