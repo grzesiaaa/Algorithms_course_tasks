9623c839bd774776b9151b37b3edfcba5c604180
fiminka <wikifyy@gmail.com>
2021-12-06 12:29:15 +0100

    Update L4_ZAD3.py

--- L4_ZAD3.py
+++ L4_ZAD3.py
@@ -25,25 +25,25 @@ Symulacje przeprowadzamy w różnych wariantach:
 """
 
 
-class Stack:
-    def __init__(self):
+class Queue:
+
+    def __init__( self):
         self.items = []
+
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
 
     def isEmpty(self):
         return self.items == []
 
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items)-1]
-
     def size(self):
         return len(self.items)
 
+    def __str__(self):
+        return str(self.items)
 
 class Participant:
     """ Participant contains participant's attributes.
