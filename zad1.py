f93fb8e8a6065af06ea572d6b5e54638b5fbfbf1
fiminka <wikifyy@gmail.com>
2021-12-19 22:32:08 +0100

    changes in raport and ex1

--- zad1.py
+++ zad1.py
@@ -17,69 +17,11 @@ def lala(n):
     b = np.array(vectors)
     print(a)
     print(b)
+    start_time = time.time()
     x = linalg.solve(a, b)
-    print(x)
-    return np.dot(a, x) == b
+    fin_time = time.time() - start_time
+    # print(x)
+    return fin_time
 
 
-print(lala(3))
-"""a = np.array([[10,2,1],
- [ 8,10,2],
- [1,2,3]])
-b= np.array([8,1,3])
-x = linalg.solve(a,b)
-print(x)
-print(np.dot(a,x) ==b)"""
-
-
-def lin_alg(n):
-    matrices = []
-    vectors = []
-
-    for i in range(2, 21):
-        a = [[random.randint(-50, 50) for col in range(i)] for row in range(i)]
-        b = [random.randint(-50, 50) for col in range(i)]
-        matrices.append(a)
-        vectors.append(b)
-
-    times = []  # measures time for .solve method
-
-    for j in range(0, len(matrices)):
-        print(len(matrices))
-        x = []
-        start_matrix = time.time()
-        for i in range(0, n):
-            print(str(linalg.det(matrices[j])))
-            if linalg.det(matrices[j]) == 0:
-                print("Determinant equal 0")
-            else:
-                print("Determinant equal: " + str(linalg.det(matrices[j])))
-                x.append(linalg.solve(matrices[j], vectors[j]))
-                print(x[i])
-                print("Result " + str(j + 2) + 'x' + str(j + 2))
-                print(np.dot(matrices[j], x[i]) == vectors[j])
-        end_matrix = time.time()
-        times.append((end_matrix - start_matrix) / 100)
-        print(times)
-
-    # Creating the bar plot
-    bars = []
-    for i in range(0, len(times)):
-        bars.append(str(i + 2) + "x" + str(i + 2))
-        # print(times[i], end=" ")  #print for check times list
-        # print(bars[i], end=" ")      # print for check bars list
-
-    y_pos = np.arange(len(times))
-
-    plt.figure(figsize=(20, 5))
-    plt.grid()
-    plt.bar(y_pos, times, color='#ffd700')
-    plt.xticks(y_pos, bars)
-    plt.xlabel('Matrix', fontsize=12, color='#323232')
-    plt.ylabel('Time of solving', fontsize=12, color='#323232')
-    plt.title('Times of solving matrices of different dimensions', fontsize=16, color='#323232')
-    plt.show()
-
-
-if __name__ == '__main__':
-    lin_alg(100)
+print(lala(300))
