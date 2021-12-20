d852aa09421935eaa7a254a4f41ef0c56cce45c4
Julia Grzegorzewska <julek0309@gmail.com>
2021-12-20 21:53:37 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,27 +1,50 @@
 import numpy as np
 from scipy import linalg
-import random
 import time
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
-def lala(n):
-    matrices = []
-    vectors = []
-
-    for i in range(1, n+1):
-        matrices = [[random.randint(-100, 100) for col in range(i)] for row in range(i)]
-        vectors = [random.randint(-100, 100) for col in range(i)]
-
-    a = np.array(matrices)
-    b = np.array(vectors)
-    print(a)
-    print(b)
+def check_time(n):
+    matrix = np.random.randint(low=-100, high=100, size=(n, n))
+    vector = np.random.randint(low=-100, high=100, size=n)
     start_time = time.time()
-    x = linalg.solve(a, b)
-    fin_time = time.time() - start_time
-    # print(x)
-    return fin_time
+    linalg.solve(matrix, vector)
+    return time.time() - start_time
+
+def plot_times(n, step):
+    x = list(range(0, n, step))
+    y = [check_time(i) for i in x]
+    plt.plot(x, y, 'm.')
+    plt.xlabel("Number of unknowns")
+    plt.ylabel("Time of solution")
+    plt.title("Complexity of calculations")
+    plt.grid()
+    plt.show()
+
+def func(x, a, b, c):
+    return a*x**2 + b*x + c
+
+def find_factors(n, step):
+    x = list(range(0, n, step))
+    y = [check_time(i) for i in x]
+    popt, pcov = curve_fit(func, x, y)
+    a = popt[0]
+    b = popt[1]
+    c = popt[2]
+    return [a,b,c]
+
+def check(n, step):
+    a = find_factors(n, step)[0]
+    b = find_factors(n, step)[1]
+    c = find_factors(n, step)[2]
+    print(find_factors(n, step))
+    x = np.arange(0, n)
+    plt.plot(x, func(x, a, b, c))
+    plot_times(n, step)
+    plt.show()
 
 
-print(lala(300))
+
+
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
